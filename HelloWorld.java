53fefbb547f8fcedb0631941794913e0cfcb2a0e
Ashish Kini <akini09@gmail.com>
2021-01-06 18:07:59 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+package introduction;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> a1 = new ArrayList<>();
+		ArrayList<String> a2 = new ArrayList<>();
+		a1.add("Hi");
+		a1.add("Bye");
+		a1.add("Tata");
+		a2.add("Asish");
+		a2.add("Nimisha");
+		a2.add("Annapoorna");
+		
+	
+		
+		a2.toArray();
+		
+		System.out.println(a2);
+	}
+}
+
+
